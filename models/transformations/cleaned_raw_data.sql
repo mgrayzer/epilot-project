49fb948548911540c2466e5bf8dd9f54f5558a31
Marlon Gray <marlongray@marlons-mbp.speedport.ip>
2024-01-07 18:18:25 +0100

    Added first transformation of data

---
+++ models/transformations/cleaned_raw_data.sql
@@ -0,0 +1,13 @@
+SELECT
+datetime_utc AS timestamp_energy_generation, 
+price_area,
+country_code,
+EIC_code,
+Plant_label,
+plant_type,
+CASE WHEN plant_type LIKE '%Fossil%' THEN 'Fossil Energy'
+     WHEN plant_type = 'Nuclear' THEN 'Nuclear Energy'
+    ELSE 'Renewable Energy' END AS plant_type_compacted,
+COALESCE(generation, 0) AS energy_generation_mw
+FROM 
+ {{source('raw_data', 'entsoe_generation_unit_raw')}}
